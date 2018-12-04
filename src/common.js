5c2114d863f0b2e6f70243be6291dac40ae918dc
Julien Mastromonaco <mastromonaco.julien@gmail.com>
2018-12-04 22:10:28 +0100

    * Add src/common.js
    Add common functions
    
    - isInteger
    - isNull
    - isString
    - stringMatch

---
+++ src/common.js
@@ -0,0 +1,43 @@
+/**
+ * Check if value is an integer.
+ * @param {any} value - Tested value.
+ * @returns {boolean}
+ */
+function isInteger (value) {
+  return Number.isInteger(value)
+}
+
+/**
+ * Check if value is strict equal to null.
+ * @param {any} value - Tested value.
+ * @returns {boolean}
+ */
+function isNull (value) {
+  return value === null
+}
+
+/**
+ * Check if value is a string.
+ * @param {any} - Tested value.
+ * @returns {boolean}
+ */
+function isString (value) {
+  return typeof value === 'string'
+}
+
+/**
+ * Check if value is string & match with 'regex'.
+ * @param {string} value - Tested value.
+ * @param {RegExp} regex - Regex format.
+ * @returns {boolean}
+ */
+function stringMatch (value, regex) {
+  return isString(value) && !isNull(value.match(regex))
+}
+
+module.exports = {
+  isInteger,
+  isNull,
+  isString,
+  stringMatch
+}
