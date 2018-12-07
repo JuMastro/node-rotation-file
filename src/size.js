ad8e4383cb5d8158420fefb21de75ca8bf79f078
Julien Mastromonaco <mastromonaco.julien@gmail.com>
2018-12-07 18:07:00 +0100

    * Add ./src/size.js to work with size
    Add function 'getTagObject()' to transform input string tag as object.
    Add function 'getBitsFromSizeObject()' to transform tag object as bits.
    Add function 'unfriendlyze()' to wrap twice precedent functions and return bits value from string tag.
    
    * Add tests for size.js

--- src/size.js
+++ src/size.js
@@ -1,3 +1,5 @@
+const { isDefined, isInteger, isRealObject } = require('./common.js')
+
 /**
  * Size units hashmap.
  * The value unit is bit.
@@ -16,4 +18,53 @@ const unitsHashmap = {
   'g': 8589934592
 }
 
+/**
+ * Get an object from a tag.
+ * @param {string} tag - Friendly stringify tag.
+ * @returns {null|object} Tag object.
+ */
+function getTagObject (tag) {
+  const parsed = tag.match(/([0-9]+)([A-Za-z])$/)
+
+  return parsed && parsed.length >= 3
+    ? { value: parseInt(parsed[1]), unit: parsed[2] }
+    : null
+}
+
+/**
+ * Get bits from a size object.
+ * @param {object} sizeObject - Size object.
+ * @returns {null|number} sizeObject in bits.
+ */
+function getBitsFromSizeObject (sizeObject) {
+  if (!sizeObject) {
+    return null
+  }
+
+  const isValidUnit = isDefined(unitsHashmap[sizeObject.unit])
+  const isValidInteger = isInteger(sizeObject.value)
+  const isValidObject = isRealObject(sizeObject)
+
+  return isValidObject && isValidInteger && isValidUnit
+    ? sizeObject.value * unitsHashmap[sizeObject.unit]
+    : null
+}
+
+/**
+ * Get bit from sizetag.
+ * First step, the sizetag is converted as sizeObject.
+ * Next, it's checked & converted to bits.
+ * @param {string} sizetag - Friendly stringify size interval.
+ * @returns {number} sizetag in bits.
+ */
+function unfriendlyze (sizetag) {
+  return getBitsFromSizeObject(getTagObject(sizetag))
+}
+
+module.exports = {
+  getTagObject,
+  getBitsFromSizeObject,
+  unfriendlyze,
+  unitsHashmap
+}
 module.exports.unitsHashmap = unitsHashmap
---
+++ test/specs/src/size.spec.js
@@ -0,0 +1,44 @@
+const path = require('path')
+
+describe('src/size.js', () => {
+  const sizeModule = require(path.resolve(__root, './src/size.js'))
+
+  test('getTagObject()', () => {
+    expect(sizeModule.getTagObject('10m')).toEqual({
+      value: 10,
+      unit: 'm'
+    })
+
+    expect(sizeModule.getTagObject('1000024m')).toEqual({
+      value: 1000024,
+      unit: 'm'
+    })
+
+    expect(sizeModule.getTagObject('1mmm')).toEqual(null)
+
+    expect(() => sizeModule.getTagObject({})).toThrowError(TypeError)
+  })
+
+  test('getBitsFromSizeObject()', () => {
+    const check = (value, unit, result) => {
+      expect(sizeModule.getBitsFromSizeObject({ value, unit })).toEqual(result)
+    }
+
+    check(10, 'b', 10)
+    check(10, 'o', 80)
+    check(10, 'k', 81920)
+    check(10, 'm', 83886080)
+    check(10, 'g', 85899345920)
+    check(10, '?', null)
+    check(undefined, 'k', null)
+  })
+
+  test('unfriendlyze()', () => {
+    expect(sizeModule.unfriendlyze('15o')).toEqual(120)
+    expect(sizeModule.unfriendlyze('10m')).toEqual(83886080)
+    expect(sizeModule.unfriendlyze('10M')).toEqual(null)
+    expect(sizeModule.unfriendlyze('10mo')).toEqual(null)
+
+    expect(() => sizeModule.unfriendlyze({})).toThrowError(TypeError)
+  })
+})
