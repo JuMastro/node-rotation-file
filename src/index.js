6786adb148e5126b39b962ef745216bc5651e14e
Julien Mastromonaco <mastromonaco.julien@gmail.com>
2018-12-10 17:30:05 +0100

    Implement methods from RotationFileStream (_init(), _open())
    * Add '_init()' method, to run stream rotation file process.
    * Add '_open()' method, to create & open subwritablestream process.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+const fs = require('fs')
 const { Writable } = require('stream')
 const { checkOptions } = require('./options.js')
+const { TracableError } = require('./errors.js')
+const fm = require('./fm.js')
 const size = require('./size.js')
 const time = require('./time.js')
 
+/**
+ * Class RotationFileStream.
+ * @extends {Writable}
+ */
 class RotationFileStream extends Writable {
   constructor (options = {}) {
     options = checkOptions(options)
@@ -24,18 +31,56 @@ class RotationFileStream extends Writable {
 
     this.once('error', this._error)
 
-    this._init()
+    this._init(1)
   }
 
   end () {
     // TODO: Should implement 'end' action.
   }
 
-  _init () {}
+  /**
+   * Method to init stream process.
+   * This method will create the directory path and the output file,
+   * before opening the substream to write.
+   * @param {number} retry - Retry count before stop.
+   * @returns {void}
+   */
+  async _init (retry = 1) {
+    try {
+      const stat = await fm.stat(this.path)
+
+      if (!stat && retry) {
+        await fm.makePath(this.path)
+        return this._init(--retry)
+      }
+
+      this.birthtime = stat.birthtime
+      this.size = stat.size
+      this._open()
+    } catch (err) {
+      this.emit('error', new TracableError(err))
+    }
+  }
 
   _rotate () {}
 
-  _open () {}
+  /**
+   * Method to create and open a substream that will do the writing to files.
+   * This method will create and open a writing stream, trace errors,
+   * and try to consume the chunks on hold using '_drain()' method.
+   * @returns {void}
+   */
+  _open () {
+    const writer = fs.createWriteStream(this.path, { flags: 'a' })
+
+    writer.once('error', (err) => {
+      this.emit('error', new TracableError(err))
+    })
+
+    writer.once('open', (fd) => {
+      this.writer = writer
+    })
+  }
 
   _close () {}
 
