dfc5ec931eba339ce956c04f8a541a817cba5706
Julien Mastromonaco <mastromonaco.julien@gmail.com>
2018-12-12 12:22:16 +0100

    Improve test/jest.utils.js

--- test/jest.utils.js
+++ test/jest.utils.js
@@ -1,25 +1,31 @@
 const fs = require('fs')
 const path = require('path')
 const { promisify } = require('util')
-const rmdir = promisify(fs.rmdir)
-const readdir = promisify(fs.readdir)
-const stat = promisify(fs.stat)
-const unlink = promisify(fs.unlink)
-const mkdir = promisify(fs.mkdir)
+
+const promised = {
+  open: promisify(fs.open),
+  close: promisify(fs.close),
+  rmdir: promisify(fs.rmdir),
+  readdir: promisify(fs.readdir),
+  stat: promisify(fs.stat),
+  unlink: promisify(fs.unlink),
+  mkdir: promisify(fs.mkdir),
+  writeFile: promisify(fs.writeFile)
+}
 
 async function removeContent (target) {
-  const items = await readdir(target)
+  const items = await promised.readdir(target)
 
   for await (const item of items) {
     const subtarget = path.resolve(target, item)
-    const info = await stat(subtarget)
-    info.isFile() ? await unlink(subtarget) : await removeDirRecursive(subtarget)
+    const info = await promised.stat(subtarget)
+    info.isFile() ? await promised.unlink(subtarget) : await removeDirRecursive(subtarget)
   }
 }
 
 async function removeDirRecursive (target) {
   try {
-    await rmdir(target)
+    await promised.rmdir(target)
   } catch (err) {
     if (err.code === 'ENOTEMPTY') {
       await removeContent(target)
@@ -35,10 +41,4 @@ const root = {
 
 module.exports = root
 module.exports.JestUtils = root
-module.exports.promised = {
-  rmdir,
-  readdir,
-  stat,
-  unlink,
-  mkdir
-}
+module.exports.promised = promised
