76a0c4aab73aeba358fc9d930a0c42a56da16a76
Julien Mastromonaco <mastromonaco.julien@gmail.com>
2018-12-12 16:24:50 +0100

    Update global coverage threshold to be a bit more friendly, get 95% instead of 100%

--- test/jest.config.js
+++ test/jest.config.js
@@ -10,10 +10,10 @@ module.exports = {
   coveragePathIgnorePatterns: ['/node_modules/'],
   coverageThreshold: {
     global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100
+      branches: 95,
+      functions: 95,
+      lines: 95,
+      statements: 95
     }
   },
   globals: {
